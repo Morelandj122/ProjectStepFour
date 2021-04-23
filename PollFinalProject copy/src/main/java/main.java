93f7e51bf2438027e5b633e6e5f184545fabbabf
naryu55101 <83037144+naryu55101@users.noreply.github.com>
2021-04-23 10:43:59 -0500

    Add files via upload

---
+++ PollFinalProject copy/src/main/java/main.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author sohambhatt
+ */
+
+import javax.swing.*;
+
+
+
+
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        JFrame f;
+        f=new JFrame();
+        JOptionPane.showMessageDialog(f," Hello: Welcome to the Poll Website \n ");
+        JOptionPane.showInputDialog(f," 1) Create Poll \n 2) Check Poll Results \n 3) Send Emergency Message ");
+        
+    }
+    
+    
+}
